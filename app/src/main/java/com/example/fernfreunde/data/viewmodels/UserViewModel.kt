1a4874b7517052d1a30d16413d526858df858ba2
jennie <jennifer.foth@smail.th-koeln.de>
2025-09-04 22:01:36 +0200

    UserViewModel

---
+++ app/src/main/java/com/example/fernfreunde/data/viewmodels/UserViewModel.kt
@@ -0,0 +1,19 @@
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class UserViewModel : ViewModel() {
+    var username by mutableStateOf("")
+    var email by mutableStateOf("")
+    var password by mutableStateOf("")
+    var profileImageUri by mutableStateOf<Uri?>(null)
+
+    fun register(username: String, email: String, password: String, image: Uri?) {
+        this.username = username
+        this.email = email
+        this.password = password
+        this.profileImageUri = image
+    }
+}
